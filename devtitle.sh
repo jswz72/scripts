9d1687bf305011ae7dfaa08131972452993d1a3f
Jacob Sword <jacobdasword@gmail.com>
2017-10-26 19:43:56 -0400

    Shebang, update comment

--- devtitle.sh
+++ devtitle.sh
@@ -1,4 +1,5 @@
-#change title of current project to DEV
+#!/bin/sh
+#change title of current project to DEV + first arg
 index=`find . -name 'index.html' | grep -v node_modules`
 echo "Index file: ${index}"
 titleline=`grep title ${index} `
